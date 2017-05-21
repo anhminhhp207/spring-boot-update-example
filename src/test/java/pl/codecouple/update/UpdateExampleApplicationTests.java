61d0f9bca3d2b017721ddc89a2ab99596573dd01
kchrusciel <krzysztof.chrusciel@outlook.com>
2017-05-21 11:25:05 +0200

    Initial commit

---
+++ src/test/java/pl/codecouple/update/UpdateExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.codecouple.update;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UpdateExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
